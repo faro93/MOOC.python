ca3628a07f92b425bab06ad53657f27608eadaa5
faro93 <fgithub@famille-rousselot.net>
2021-01-15 23:12:37 +0100

    fibonacci.OK

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import sys
 
 def fibonacci (n):
     if n <= 1:
@@ -6,7 +8,26 @@ def fibonacci (n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def usage(e):
+    print(f"Usage : {sys.argv[0]} <int>")
+
+def is_integer(n):
+    try:
+        int(n)
+    except ValueError:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
-    for i in range(8):
-        print(f'fibonacci({i}) : {fibonacci(i)}')
+    l = len(sys.argv)
+    if l == 2:
+        n = sys.argv[1]
+        if is_integer(n):
+            n = int(n)
+            print(fibonacci(n))
+        else:
+            usage(1)
+    else:
+        usage(0)
+    
