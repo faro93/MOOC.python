b90766b37eb237ec3430d71650865b013b20db89
faro93 <fgithub@famille-rousselot.net>
2021-01-15 22:32:32 +0100

    202101142232.fibonacci.typo

--- fibonacci.py
+++ fibonacci.py
@@ -4,9 +4,9 @@ def fibonacci (n):
     if n <= 1:
         return n
     else:
-        return fibonacci(n-1)+ fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 if __name__ == '__main__':
     for i in range(8):
-        print(f'{i} : {fibonacci(i)}')
+        print(f'fibonacci({i}) : {fibonacci(i)}')
