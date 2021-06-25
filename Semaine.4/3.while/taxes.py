320f0c08743642a78649ea15a5a8bee62ee94c35
faro93 <fgithub@famille-rousselot.net>
2021-06-26 00:26:49 +0200

    202106260025_taxes.py_in_progress

--- Semaine.4/3.while/taxes.py
+++ Semaine.4/3.while/taxes.py
@@ -1,15 +1,30 @@
 # code utf-8
 
 def taxes(income):
-    print(income)
+    tranches = [150_000, 50_000, 12_500]
+    taux = [0.45, 0.4, 0.2]
+    c = 0
+    # print(f'0.income = {income}')
     reste = income
-    impot = int()
-    while reste > 0:
-        if reste < 12_500:
-            return impot
-        elif 12_501 <= reste <= 50_000:
-            reste -= 1
+    intermediaire = income
+    restes = list()
+
+    while reste >= 0 and c < len(tranches):
+        if reste >= tranches[c]:
+            reste = intermediaire - tranches[c]
+            intermediaire = intermediaire - reste
+            restes.append((reste, taux[c]))
+            print(f'1.tranche{c+1}(>{tranches[c]}) : {reste}, intermediaire = {intermediaire}')
+        c += 1
+    print(restes)
+
+    impot = float()
+    for (a, b) in restes:
+        impot = impot + a * b
+    return(int(impot))
 
 if __name__ == '__main__':
-    for taxe in (0, 7500, 12500):
-        taxes(taxe)
+    # for montant in (0, 50_000, 12_500, 5_000, 16_500, 30_000, 100_000, 150_000, 200_000, 12_504):
+    for montant in ([50_000, 200_000]):
+        print(montant, taxes(montant))
+        print()
