c0ae8da037efa3549ab3b03715ffd71ecb060790
faro93 <fgithub@famille-rousselot.net>
2021-06-28 17:10:59 +0200

    202106281700-spreadsheat.py_begin

---
+++ Semaine.4/3.while/spreadsheet.py
@@ -0,0 +1,12 @@
+# code utf-8
+
+def spreadsheet(index):
+    i = int()
+    s = str()
+    while index > 0:
+        pass
+    return(index, s)
+
+if __name__ == '__main__':
+    for i in (1, 15, 26, 27, 701, 702, 703, 704, 18277, 18278, 18279, 18280, 675, 30_000, 100_000, 1_000_000):
+        print(spreadsheet(i))
