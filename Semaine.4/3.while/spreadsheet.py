d8b038ea0adb31efb8be88ed255707648b0f474a
faro93 <fgithub@famille-rousselot.net>
2021-08-03 23:02:36 +0200

    202108032300.spreadsheet.py

--- Semaine.4/3.while/spreadsheet.py
+++ Semaine.4/3.while/spreadsheet.py
@@ -24,36 +24,36 @@ def spreadsheet(n):
         return s
 
 def spreadsheet2(n):
-    s = str()
+    s = list()
     alphasize = 26
     A = 65
+    qA = 64
     if n == 0:
         return None
     else:
         n -= 1
         if n > alphasize-1:
-            q = n//(alphasize)
-            r = n % (alphasize)
-            while (q > alphasize):
-                print(f'1.q > alphasize ', end = '')
-                
-                break
-            qc = 64
-            q1 = q+qc
+            q = n//alphasize
+            qcur = q
+            while (qcur > alphasize):
+                s.append(chr(qcur%alphasize+qA))
+                qcur = qcur//alphasize
+            r = n % alphasize
             r1 = r+A
-            s += chr(q1) + chr(r1)
+            s.append(chr(qcur+qA))
+            s.append(chr(r1))
             n += 1
-            print(f'n={n}, q={q}, r={r}, s={s}(chr({q}+{qc}={q1}) + chr({r}+{A}={r1})) --> ', end = '')
+            # print(f'n={n}, q={q}, r={r}, s={s}(chr({q}+{qc}={q1}) + chr({r}+{A}={r1})) --> ', end = '')
         else:
             s = chr(n+A)
             n += 1
 
-        return s
+        return ''.join(s)
 
 if __name__ == '__main__':
     # for i in range(0, 84):
     #     print(spreadsheet(i))
-    for i in (1, 15, 26, 27, 701, 702, 703):
+    for i in range(600, 801):
         print(f'{i}, {spreadsheet2(i)}')
     
     # for i in (1, 15, 26, 27, 701, 702, 703, 704, 18277, 18278, 18279, 18280, 675, 30_000, 100_000, 1_000_000):
