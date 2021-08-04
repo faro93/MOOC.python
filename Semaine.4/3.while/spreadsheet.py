75bffef8c2f46e8508ff4ff6ad3a6868fdb55ec2
faro93 <fgithub@famille-rousselot.net>
2021-08-04 17:00:48 +0200

    202108041700.spreadsheet.py

--- Semaine.4/3.while/spreadsheet.py
+++ Semaine.4/3.while/spreadsheet.py
@@ -33,30 +33,51 @@ def spreadsheet2(n):
     else:
         n -= 1
         if n > alphasize-1:
-            q = n//alphasize
-            qcur = q
+            q1 = n//alphasize
+            qcur = q1
             while (qcur > alphasize):
                 s.append(chr(qcur%alphasize+qA))
                 qcur = qcur//alphasize
-            r = n % alphasize
-            r1 = r+A
             s.append(chr(qcur+qA))
-            s.append(chr(r1))
+            s.append(chr(n%alphasize+A))
             n += 1
-            # print(f'n={n}, q={q}, r={r}, s={s}(chr({q}+{qc}={q1}) + chr({r}+{A}={r1})) --> ', end = '')
         else:
             s = chr(n+A)
             n += 1
 
         return ''.join(s)
 
+# index	q1=index/26     r1=index%26     q2=q1/26    r2=q1%26	CHAR	attendu	formule
+# 0     0               0                                       A       A       (r1+65)
+# 25  	0   	        25          		        	        Z	    Z       "
+# 26    1           	0           		        	        AA	    AA	    (q1+64)&(r1+65)
+# 27    1           	1           		        	        AB	    AB      "
+# 701 	26          	25          		        	        ZZ	    ZZ	    "
+# 702 	27          	0           	1       	1	        AAA	    AAA	    (q2+64)&(r2+64)&(r1+65)
+# 727 	27          	25          	1       	1	        AAZ	    AAZ	    "
+# 728 	28          	0           	1       	2	        ABA	    ABA	    "
+# 753 	28          	25          	1       	2	        ABZ	    ABZ	    "
+# 754 	29          	0           	1       	3	        ACA	    ACA	    "
+# 755 	29          	1           	1	        3	        ACB	    ACB	    "
+
+
 if __name__ == '__main__':
-    # for i in range(0, 84):
-    #     print(spreadsheet(i))
-    for i in range(600, 801):
-        print(f'{i}, {spreadsheet2(i)}')
+    # for i in range(1, 6):
+    #     print(f'{i}, {spreadsheet2(i)}')
+    # print('################')
+    # for i in range(26, 28):
+    #     print(f'{i}, {spreadsheet2(i)}')
+    # print('################')
+    for i in range(702, 704):
+        print(f'{i-1}, {spreadsheet2(i)}')
+    print('################')
+    for i in range(728, 730):
+        print(f'{i-1}, {spreadsheet2(i)}')
+    print('################')
+    for i in range(754, 757):
+        print(f'{i-1}, {spreadsheet2(i)}')
     
     # for i in (1, 15, 26, 27, 701, 702, 703, 704, 18277, 18278, 18279, 18280, 675, 30_000, 100_000, 1_000_000):
-    #     print(spreadsheet(i))
+    #     print(f'{i}, {spreadsheet2(i)}')
 
     print(f'*** chr(64)={chr(64)}, chr(65)={chr(65)}, chr(90)={chr(90)}, chr(91)={chr(91)} ***')
