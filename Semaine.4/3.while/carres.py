de74cf87be811eb92de758eb47bc41e4f6c03dc5
faro93 <fgithub@famille-rousselot.net>
2021-08-08 21:16:12 +0200

    202108082113.carres.py

---
+++ Semaine.4/3.while/carres.py
@@ -0,0 +1,17 @@
+# code utf-8
+# log a(b) = c => b = a**c
+# log2(16)= 4 => 16 = 4**2
+# log(10_000)= 4 => 10_000 = 10**4
+# log(1)= 0 => 1 = 10**0
+# log3(27)= 3 => 27 = 3**3
+# log(0.1)= -1 => 1 = 10**-1
+# log(1/100)= -2 => 1/100 = 10**-2
+# log(5)~= 0.699 => 5 = 10**0.699
+
+def power(x, n):
+    print(f'x={x}, y={y} ')
+    pass
+
+if __name__ == "__main__":
+    for (i, j) in enumerate(range(50)):
+        print(f'{i}**{j} = {power(i, j)}')
